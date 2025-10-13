90b5439219aef1f78151bff50a1bed8c394f3bc9
MohamedFares09 <mofares010@gmail.com>
2025-10-13 14:39:55 +0300

    add imagerepo

---
+++ lib/core/repos/images_repo/image_repo_impl.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+import 'package:fruit_app_dashboard/core/repos/images_repo/image_repo.dart';
+
+class ImageRepoImpl implements ImageRepo {
+  @override
+  Future<void> uploadImage(File image) {
+    // TODO: implement uploadImage
+    throw UnimplementedError();
+  }
+
+}
