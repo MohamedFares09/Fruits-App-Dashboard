e90ef4913323d5db2b3589252d9db9b1e6facb94
MohamedFares09 <mofares010@gmail.com>
2026-01-31 13:41:00 +0200

    add product cubit and add functions

---
+++ lib/features/add_product/presentation/manager/add_product/add_product_cubit.dart
@@ -0,0 +1,27 @@
+import 'package:bloc/bloc.dart';
+import 'package:fruit_app_dashboard/core/repos/images_repo/image_repo.dart';
+import 'package:fruit_app_dashboard/core/repos/product_repo/product_repo.dart';
+import 'package:fruit_app_dashboard/features/add_product/doman/entities/add_product_entity.dart';
+import 'package:meta/meta.dart';
+
+part 'add_product_state.dart';
+
+class AddProductCubit extends Cubit<AddProductState> {
+  AddProductCubit(this.imageRepo, this.productRepo)
+    : super(AddProductInitial());
+  final ImageRepo imageRepo;
+  final ProductRepo productRepo;
+  Future<void> addProduct(AddProductEntity addProductEntity) async {
+    emit(AddProductLoading());
+    var result = await imageRepo.uploadImage(addProductEntity.image);
+    result.fold((failure) => emit(AddProductFailure(failure.message)), (
+      url,
+    ) async {
+      addProductEntity.urlImage = url;
+      var result = await productRepo.addProduct(addProductEntity);
+      result.fold((failure) => emit(AddProductFailure(failure.message)), (r) {
+        emit(AddProductSuccess());
+      });
+    });
+  }
+}
