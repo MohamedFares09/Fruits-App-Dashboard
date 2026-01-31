b320d611e4dc43953ae591a4ef970e89e504399b
MohamedFares09 <mofares010@gmail.com>
2026-01-31 13:40:22 +0200

    edit product repo

--- lib/core/repos/product_repo/product_repo.dart
+++ lib/core/repos/product_repo/product_repo.dart
@@ -1,3 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:fruit_app_dashboard/core/errors/failuer.dart';
+import 'package:fruit_app_dashboard/features/add_product/doman/entities/add_product_entity.dart';
+
 abstract class ProductRepo {
-  Future<void> addProduct(Map<String, dynamic> productData);
+  Future<Either<Failure, void>> addProduct(AddProductEntity addProductEntity);
 }
