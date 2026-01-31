a02fe623f255246e85af3d8d303e2254213b4e90
MohamedFares09 <mofares010@gmail.com>
2026-01-31 13:40:10 +0200

    add get it  function

--- lib/core/services/get_it.dart
+++ lib/core/services/get_it.dart
@@ -1,3 +1,7 @@
+import 'package:fruit_app_dashboard/core/repos/images_repo/image_repo.dart';
+import 'package:fruit_app_dashboard/core/repos/images_repo/image_repo_impl.dart';
+import 'package:fruit_app_dashboard/core/repos/product_repo/product_repo.dart';
+import 'package:fruit_app_dashboard/core/repos/product_repo/product_repo_impl.dart';
 import 'package:fruit_app_dashboard/core/services/file_storage.dart';
 import 'package:fruit_app_dashboard/core/services/storage_services.dart';
 import 'package:get_it/get_it.dart';
@@ -6,4 +10,8 @@ final getit = GetIt.instance;
 
 void setupGetIt() {
   getit.registerLazySingleton<StorageServices>(() => FileStorage());
+  getit.registerLazySingleton<ImageRepo>(
+    () => ImageRepoImpl(getit.get<StorageServices>()),
+  );
+  getit.registerLazySingleton<ProductRepo>(() => ProductRepoImpl());
 }
