18b4ae17c6238bf202bd335889dc543adc4781ff
MohamedFares09 <mofares010@gmail.com>
2026-02-05 15:10:02 +0200

    add firestore_services

---
+++ lib/core/services/firestore_services.dart
@@ -0,0 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fruit_app_dashboard/core/services/database_services.dart';
+
+
+class FireStoreServices implements DataBaseServices {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  @override
+  Future<void> addData(
+      {required String path,
+      required Map<String, dynamic> data,
+      String? documentId}) async {
+    if (documentId != null) {
+      await firestore.collection(path).doc(documentId).set(data);
+    } else {
+      await firestore.collection(path).add(data);
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> getUserData(
+      {required String path, required String uid}) async {
+    var data = await firestore.collection(path).doc(uid).get();
+    return data.data() as Map<String, dynamic>;
+  }
+
+  @override
+  Future<bool> isExisits(
+      {required String path, required String documentId}) async {
+    var data = await firestore.collection(path).doc(documentId).get();
+    return data.exists;
+  }
+}
