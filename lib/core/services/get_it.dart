35eac6ba91685e9fab851d100b079c560952aec4
MohamedFares09 <mofares010@gmail.com>
2026-02-05 16:05:06 +0200

    add getit DataBaseServices

--- lib/core/services/get_it.dart
+++ lib/core/services/get_it.dart
@@ -2,7 +2,9 @@ import 'package:fruit_app_dashboard/core/repos/images_repo/image_repo.dart';
 import 'package:fruit_app_dashboard/core/repos/images_repo/image_repo_impl.dart';
 import 'package:fruit_app_dashboard/core/repos/product_repo/product_repo.dart';
 import 'package:fruit_app_dashboard/core/repos/product_repo/product_repo_impl.dart';
+import 'package:fruit_app_dashboard/core/services/database_services.dart';
 import 'package:fruit_app_dashboard/core/services/file_storage.dart';
+import 'package:fruit_app_dashboard/core/services/firestore_services.dart';
 import 'package:fruit_app_dashboard/core/services/storage_services.dart';
 import 'package:get_it/get_it.dart';
 
@@ -10,8 +12,11 @@ final getit = GetIt.instance;
 
 void setupGetIt() {
   getit.registerLazySingleton<StorageServices>(() => FileStorage());
+  getit.registerLazySingleton<DataBaseServices>(() => FireStoreServices());
   getit.registerLazySingleton<ImageRepo>(
     () => ImageRepoImpl(getit.get<StorageServices>()),
   );
-  getit.registerLazySingleton<ProductRepo>(() => ProductRepoImpl());
+  getit.registerLazySingleton<ProductRepo>(
+    () => ProductRepoImpl(getit.get<DataBaseServices>()),
+  );
 }
