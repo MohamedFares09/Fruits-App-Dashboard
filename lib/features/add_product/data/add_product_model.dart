8b111b197111fae23c81f077b534a9db00771899
MohamedFares09 <mofares010@gmail.com>
2026-02-05 16:04:08 +0200

    create addproduct model

---
+++ lib/features/add_product/data/add_product_model.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+import 'package:fruit_app_dashboard/features/add_product/doman/entities/add_product_entity.dart';
+
+class AddProductModel {
+  final String name;
+  final String code;
+  final String description;
+  final num price;
+  final File image;
+  final bool isFeatured;
+  String ? urlImage; 
+  AddProductModel({
+    required this.name,
+    required this.code,
+    required this.description,
+    required this.price,
+    required this.image,
+    required this.isFeatured,
+     this.urlImage,
+  });
+  factory AddProductModel.fromEntity(AddProductEntity entity){
+    return AddProductModel(
+      name: entity.name,
+      code: entity.code,
+      description: entity.description,
+      price: entity.price,
+      image: entity.image,
+      isFeatured: entity.isFeatured,
+      urlImage: entity.urlImage,
+    );
+  }
+  toJson(){
+    return {
+      'name': name,
+      'code': code,
+      'description': description,
+      'price': price,
+      'image': image,
+      'isFeatured': isFeatured,
+      'urlImage': urlImage,
+    };
+  }
+}
