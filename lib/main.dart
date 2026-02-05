699c4303a2c1eeb7584f687fe1467d6a9a682828
MohamedFares09 <mofares010@gmail.com>
2026-02-05 19:45:02 +0200

    add bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruit_app_dashboard/core/helper_function/on_generate_route.dart';
+import 'package:fruit_app_dashboard/core/services/custom_bloc_observer.dart';
+import 'package:fruit_app_dashboard/core/services/get_it.dart';
 import 'package:fruit_app_dashboard/features/dashboard/presentation/views/dashboard_view.dart';
+import 'package:fruit_app_dashboard/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  Bloc.observer = CustomBlocObserver();
+  setupGetIt();
+
   runApp(const MainApp());
 }
 
