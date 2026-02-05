13b3589d277c801fa501c1f0435b2aba89004af5
MohamedFares09 <mofares010@gmail.com>
2026-02-05 16:04:41 +0200

    impl product repo impl

--- lib/core/repos/product_repo/product_repo_impl.dart
+++ lib/core/repos/product_repo/product_repo_impl.dart
@@ -1,9 +1,27 @@
+import 'package:dartz/dartz.dart';
+import 'package:fruit_app_dashboard/core/errors/failuer.dart';
 import 'package:fruit_app_dashboard/core/repos/product_repo/product_repo.dart';
+import 'package:fruit_app_dashboard/core/services/database_services.dart';
+import 'package:fruit_app_dashboard/core/utils/back_end.dart';
+import 'package:fruit_app_dashboard/features/add_product/data/add_product_model.dart';
+import 'package:fruit_app_dashboard/features/add_product/doman/entities/add_product_entity.dart';
 
-class ProductRepoImpl  implements ProductRepo{
+class ProductRepoImpl implements ProductRepo {
+  final DataBaseServices dataBaseServices;
+
+  ProductRepoImpl(this.dataBaseServices);
   @override
-  Future<void> addProduct(Map<String, dynamic> productData) {
-    throw UnimplementedError();
+  Future<Either<Failure, void>> addProduct(
+    AddProductEntity addProductEntity,
+  ) async {
+    try {
+      await dataBaseServices.addData(
+        path: BackEnd.addProducts,
+        data: AddProductModel.fromEntity(addProductEntity).toJson(),
+      );
+      return right(null);
+    } catch (e) {
+      return left(ServerFailure("Something went wrong"));
+    }
   }
-  
 }
--- lib/core/services/database_services.dart
+++ lib/core/services/database_services.dart
@@ -1,5 +1,15 @@
 abstract class DataBaseServices {
-  Future<void> addData({ required String path, required Map<String, dynamic> data ,  String? documentId });
-  Future<Map<String , dynamic>> getUserData({required String path, required String uid});
-  Future<bool> isExisits({required String path , required String documentId});
+  Future<void> addData({
+    required String path,
+    required Map<String, dynamic> data,
+    String? documentId,
+  });
+  Future<Map<String, dynamic>> getUserData({
+    required String path,
+    required String uid,
+  });
+  Future<bool> isExisits({
+    required String path,
+    required String documentId,
+  });
 }
