3c0f7928bd6dca8f5d7db26bae2eff45103c1235
MohamedFares09 <mofares010@gmail.com>
2026-02-08 18:46:32 +0200

    add bloc consumer

--- lib/features/add_product/presentation/widgets/add_product_view_body.dart
+++ lib/features/add_product/presentation/widgets/add_product_view_body.dart
@@ -3,11 +3,13 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruit_app_dashboard/core/utils/widgets/custom_button.dart';
+import 'package:fruit_app_dashboard/core/utils/widgets/custom_progress_hud.dart';
 import 'package:fruit_app_dashboard/core/utils/widgets/custom_text_form_field.dart';
 import 'package:fruit_app_dashboard/features/add_product/doman/entities/add_product_entity.dart';
 import 'package:fruit_app_dashboard/features/add_product/presentation/manager/add_product/add_product_cubit.dart';
 import 'package:fruit_app_dashboard/features/add_product/presentation/widgets/image_filed.dart';
 import 'package:fruit_app_dashboard/features/add_product/presentation/widgets/IsFeaturedBox.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class AddProductViewBody extends StatefulWidget {
   const AddProductViewBody({super.key});
@@ -31,82 +33,92 @@ class _AddProductViewBodyState extends State<AddProductViewBody> {
         autovalidateMode: autovalidateMode,
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 18.0),
-          child: Column(
-            children: [
-              CustomTextFormField(
-                hintText: 'Product Name',
-                keyboardType: TextInputType.text,
-                onSaved: (value) {
-                  name = value!;
-                },
-              ),
-              SizedBox(height: 16),
-              CustomTextFormField(
-                hintText: 'Product Price',
-                keyboardType: TextInputType.number,
-                onSaved: (value) {
-                  price = double.parse(value!);
-                },
-              ),
-              SizedBox(height: 16),
-              CustomTextFormField(
-                hintText: 'product code',
-                keyboardType: TextInputType.text,
-                onSaved: (value) {
-                  code = value!.toLowerCase();
-                },
-              ),
-              // Save product code,
-              SizedBox(height: 16),
-              CustomTextFormField(
-                hintText: 'Product Description',
-                maxLines: 5,
-                keyboardType: TextInputType.multiline,
-                onSaved: (value) {
-                  description = value!;
-                },
-              ),
-              SizedBox(height: 16),
-              IsFeaturedCheckbox(
-                onChanged: (value) {
-                  isFeatured = value;
-                },
-              ),
-              SizedBox(height: 16),
-              ImageField(
-                onFileChanged: (value) {
-                  setState(() {
-                    image = value;
-                  });
-                },
-              ),
-              SizedBox(height: 24),
-              CustomButton(
-                text: 'Add Product',
-                onPressed: () {
-                  if (image != null) {
-                    if (formKey.currentState!.validate()) {
-                      formKey.currentState!.save();
-                      AddProductEntity input = AddProductEntity(
-                        name: name,
-                        code: code,
-                        description: description,
-                        price: price,
-                        image: image!,
-                        isFeatured: isFeatured,
-                      );
-                      context.read<AddProductCubit>().addProduct(input);
-                    } else {
-                      autovalidateMode = AutovalidateMode.always;
-                      setState(() {});
-                    }
-                  } else {
-                    showErrorBar(context, 'Please select an image');
-                  }
-                },
-              ),
-              SizedBox(height: 24),
-            ],
+          child: BlocConsumer<AddProductCubit, AddProductState>(
+            listener: (context, state) {
+              // TODO: implement listener
+            },
+            builder: (context,  state) {
+              return CustomProgressHUD(
+                isLoading: state is AddProductLoading,
+                child: Column(
+                  children: [
+                    CustomTextFormField(
+                      hintText: 'Product Name',
+                      keyboardType: TextInputType.text,
+                      onSaved: (value) {
+                        name = value!;
+                      },
+                    ),
+                    SizedBox(height: 16),
+                    CustomTextFormField(
+                      hintText: 'Product Price',
+                      keyboardType: TextInputType.number,
+                      onSaved: (value) {
+                        price = double.parse(value!);
+                      },
+                    ),
+                    SizedBox(height: 16),
+                    CustomTextFormField(
+                      hintText: 'product code',
+                      keyboardType: TextInputType.text,
+                      onSaved: (value) {
+                        code = value!.toLowerCase();
+                      },
+                    ),
+                    // Save product code,
+                    SizedBox(height: 16),
+                    CustomTextFormField(
+                      hintText: 'Product Description',
+                      maxLines: 5,
+                      keyboardType: TextInputType.multiline,
+                      onSaved: (value) {
+                        description = value!;
+                      },
+                    ),
+                    SizedBox(height: 16),
+                    IsFeaturedCheckbox(
+                      onChanged: (value) {
+                        isFeatured = value;
+                      },
+                    ),
+                    SizedBox(height: 16),
+                    ImageField(
+                      onFileChanged: (value) {
+                        setState(() {
+                          image = value;
+                        });
+                      },
+                    ),
+                    SizedBox(height: 24),
+                    CustomButton(
+                      text: 'Add Product',
+                      onPressed: () {
+                        if (image != null) {
+                          if (formKey.currentState!.validate()) {
+                            formKey.currentState!.save();
+                            AddProductEntity input = AddProductEntity(
+                              name: name,
+                              code: code,
+                              description: description,
+                              price: price,
+                              image: image!,
+                              isFeatured: isFeatured,
+                            );
+                            context.read<AddProductCubit>().addProduct(input);
+                          } else {
+                            autovalidateMode = AutovalidateMode.always;
+                            setState(() {});
+                          }
+                        } else {
+                          showErrorBar(context, 'Please select an image');
+                        }
+                      },
+                    ),
+                    SizedBox(height: 24),
+                  ],
+                ),
+              );
+            },
           ),
         ),
       ),
