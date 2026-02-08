fc28544b8eb3699543921529c54498b060a1a8b0
MohamedFares09 <mofares010@gmail.com>
2026-02-08 19:29:16 +0200

    fix bug in add product view

--- lib/features/add_product/presentation/widgets/add_product_view_body.dart
+++ lib/features/add_product/presentation/widgets/add_product_view_body.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_app_dashboard/core/helper_function/build_snak_bar.dart';
 import 'package:fruit_app_dashboard/core/utils/widgets/custom_button.dart';
 import 'package:fruit_app_dashboard/core/utils/widgets/custom_progress_hud.dart';
 import 'package:fruit_app_dashboard/core/utils/widgets/custom_text_form_field.dart';
@@ -9,7 +10,6 @@ import 'package:fruit_app_dashboard/features/add_product/doman/entities/add_prod
 import 'package:fruit_app_dashboard/features/add_product/presentation/manager/add_product/add_product_cubit.dart';
 import 'package:fruit_app_dashboard/features/add_product/presentation/widgets/image_filed.dart';
 import 'package:fruit_app_dashboard/features/add_product/presentation/widgets/IsFeaturedBox.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class AddProductViewBody extends StatefulWidget {
   const AddProductViewBody({super.key});
@@ -23,25 +23,43 @@ class _AddProductViewBodyState extends State<AddProductViewBody> {
   late double price;
   File? image;
   bool isFeatured = false;
+
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Form(
-        key: formKey,
-        autovalidateMode: autovalidateMode,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 18.0),
-          child: BlocConsumer<AddProductCubit, AddProductState>(
-            listener: (context, state) {
-              // TODO: implement listener
-            },
-            builder: (context,  state) {
-              return CustomProgressHUD(
-                isLoading: state is AddProductLoading,
+    return BlocConsumer<AddProductCubit, AddProductState>(
+      listener: (context, state) {
+        if (state is AddProductSuccess) {
+          buildsnakbar(
+            context,
+            'Product added successfully',
+            Colors.green,
+          );
+        }
+
+        if (state is AddProductFailure) {
+          buildsnakbar(
+            context,
+            state.errorMessage,
+            Colors.red,
+          );
+        }
+      },
+      builder: (context, state) {
+        return CustomProgressHUD(
+          isLoading: state is AddProductLoading,
+          child: SingleChildScrollView(
+            child: Form(
+              key: formKey,
+              autovalidateMode: autovalidateMode,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 18.0),
                 child: Column(
                   children: [
+                    const SizedBox(height: 24),
+
                     CustomTextFormField(
                       hintText: 'Product Name',
                       keyboardType: TextInputType.text,
@@ -49,7 +67,9 @@ class _AddProductViewBodyState extends State<AddProductViewBody> {
                         name = value!;
                       },
                     ),
-                    SizedBox(height: 16),
+
+                    const SizedBox(height: 16),
+
                     CustomTextFormField(
                       hintText: 'Product Price',
                       keyboardType: TextInputType.number,
@@ -57,16 +77,19 @@ class _AddProductViewBodyState extends State<AddProductViewBody> {
                         price = double.parse(value!);
                       },
                     ),
-                    SizedBox(height: 16),
+
+                    const SizedBox(height: 16),
+
                     CustomTextFormField(
-                      hintText: 'product code',
+                      hintText: 'Product Code',
                       keyboardType: TextInputType.text,
                       onSaved: (value) {
                         code = value!.toLowerCase();
                       },
                     ),
-                    // Save product code,
-                    SizedBox(height: 16),
+
+                    const SizedBox(height: 16),
+
                     CustomTextFormField(
                       hintText: 'Product Description',
                       maxLines: 5,
@@ -75,13 +98,17 @@ class _AddProductViewBodyState extends State<AddProductViewBody> {
                         description = value!;
                       },
                     ),
-                    SizedBox(height: 16),
+
+                    const SizedBox(height: 16),
+
                     IsFeaturedCheckbox(
                       onChanged: (value) {
                         isFeatured = value;
                       },
                     ),
-                    SizedBox(height: 16),
+
+                    const SizedBox(height: 16),
+
                     ImageField(
                       onFileChanged: (value) {
                         setState(() {
@@ -89,14 +116,17 @@ class _AddProductViewBodyState extends State<AddProductViewBody> {
                         });
                       },
                     ),
-                    SizedBox(height: 24),
+
+                    const SizedBox(height: 24),
+
                     CustomButton(
                       text: 'Add Product',
                       onPressed: () {
                         if (image != null) {
                           if (formKey.currentState!.validate()) {
                             formKey.currentState!.save();
-                            AddProductEntity input = AddProductEntity(
+
+                            final input = AddProductEntity(
                               name: name,
                               code: code,
                               description: description,
@@ -104,30 +134,37 @@ class _AddProductViewBodyState extends State<AddProductViewBody> {
                               image: image!,
                               isFeatured: isFeatured,
                             );
-                            context.read<AddProductCubit>().addProduct(input);
+
+                            context
+                                .read<AddProductCubit>()
+                                .addProduct(input);
                           } else {
                             autovalidateMode = AutovalidateMode.always;
                             setState(() {});
                           }
                         } else {
-                          showErrorBar(context, 'Please select an image');
+                          _showErrorBar(
+                            context,
+                            'Please select an image',
+                          );
                         }
                       },
                     ),
-                    SizedBox(height: 24),
+
+                    const SizedBox(height: 32),
                   ],
                 ),
-              );
-            },
+              ),
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 
-  dynamic showErrorBar(BuildContext context, String message) {
-    return ScaffoldMessenger.of(
-      context,
-    ).showSnackBar(SnackBar(content: Text(message)));
+  void _showErrorBar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message)),
+    );
   }
 }
